d49f68ca90185d6c10f2882cc6cf892c6d28bb3d
balabub <balabub@gmx.net>
2011-06-30 15:52:28 +0200

    Schachuhrfiles das erste Mal hochgeladen

---
+++ Schachuhr/Schachuhr.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-06-30T14:41:40
+#
+#-------------------------------------------------
+
+QT       += testlib
+
+TARGET = Schachuhr
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        schachuhr.cpp
+
+HEADERS  += schachuhr.h
+
+FORMS    += schachuhr.ui
---
+++ Schachuhr/schachuhr.cpp
@@ -0,0 +1,52 @@
+#include "schachuhr.h"
+#include "ui_schachuhr.h"
+
+Schachuhr::Schachuhr(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Schachuhr)
+{
+    ui->setupUi(this);
+}
+
+Schachuhr::~Schachuhr()
+{
+    delete ui;
+}
+
+void Schachuhr::changeEvent(QEvent *e)
+{
+    QMainWindow::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
+
+void Schachuhr::on_player1Button_clicked()
+{
+
+}
+
+void Schachuhr::on_player2Button_clicked()
+{
+
+}
+
+
+void Schachuhr::on_clearButton_clicked()
+{
+
+}
+
+void Schachuhr::on_quitButton_clicked()
+{
+
+}
+
+void Schachuhr::on_stopButton_clicked()
+{
+
+}
